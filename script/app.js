3b9adc2a07f4268eaa30c1e917d2a234485daae2
Codrin Diaconu <diaconu.codrin@gmail.com>
2015-12-11 04:51:06 +0000

    Exercise #: 3

--- script/app.js
+++ script/app.js
@@ -1,1 +1,7 @@
-// Write your code here!
+$('#signup-form').on('submit', function(event) {
+    var valuePassowrd = $('input[name="password"]').val();
+    var valueConfirm = $('input[name="password2"]').val();
+    if(valuePassowrd.length < 10 || valueConfirm !== valuePassowrd || valueConfirm === null || valuePassowrd === null ){
+        event.preventDefault();
+    }
+});
