a9a6e74330f1bba7621c01b7d8541ff101d20ace
Codrin Diaconu <diaconu.codrin@gmail.com>
2015-12-12 23:58:32 +0000

    Exercise #: 5 Follow the ISS with custom marker and all!

--- script/app.js
+++ script/app.js
@@ -1,25 +1,31 @@
 $('h2').after('<div class="issPos"></div>');
 $('.issPos').append('<div class="lat"><p>Latitude</p><p></p></div>');
 $('.issPos').append('<div class="lng"><p>Longitude</p><p></p></div>');
+var apiKey = 'AIzaSyBCL8IYGbVAgeymLwN9VFw2RW-kC262O7E';
+var customIcon = 'http://megaicons.net/static/img/icons_sizes/258/926/64/satellite-icon.png';
+
+setInterval(function() {
+    var parsedResponse, lat, lng;
+    $.ajax({
+        url: 'https://jquery-workshop-codus.c9users.io/',
+        jsonp: "callback",
+        dataType: "jsonp",
+        data: {
+            format: "json"
+        },
+        success: function(response) {
+            parsedResponse = JSON.parse(response);
+            lat = parsedResponse.iss_position.latitude;
+            lng = parsedResponse.iss_position.longitude;
+            $('.lat').find('p').next().text(lat);
+            $('.lng').find('p').next().text(lng);
+            var apiMap = 'https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&zoom=3&size=640x400&markers=icon:'+customIcon+'|'+lat+','+lng+'&key='+apiKey;
+            $('#issPosition').attr('src', apiMap);
+        }
+    });
+}, 5000);
 
 
 
 
-// setInterval(function(res) {
-//     var parsedResponse, lat, lng;
-//     $.ajax({
-//         url: 'https://jquery-workshop-codus.c9users.io/',
-//         jsonp: "callback",
-//         dataType: "jsonp",
-//         data: {
-//             format: "json"
-//         },
-//         success: function(response) {
-//             parsedResponse = JSON.parse(response);
-//             lat = parsedResponse.iss_position.latitude;
-//             lng = parsedResponse.iss_position.longitude;
-//             $('.lat').find('p').next().text(lat);
-//             $('.lng').find('p').next().text(lng);
-//         }
-//     });
-// }, 2000);
+
